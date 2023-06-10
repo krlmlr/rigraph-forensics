80897e8a8499dcfa62338ffe44b681011868db81
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-10 06:56:25 +0200

    Collect for all versions

--- bionic/01-collect.R
+++ bionic/01-collect.R
@@ -4,76 +4,12 @@ options(conflicts.policy = list(warn = FALSE))
 
 library(purrr)
 
-version_tags <- readRDS("bionic/tags.rds")
-# 1.4.3
-# 1.4.2
-# 1.4.1
-# 1.4.0
-# 1.3.5
-# 1.3.4
-# 1.3.3
-# 1.3.2
-# 1.3.1
-# 1.3.0
-# 1.2.11
-# 1.2.10
-# 1.2.9
-# 1.2.8
-# 1.2.7
-# 1.2.6
-# 1.2.5
-# 1.2.4.2
-# 1.2.4.1
-# 1.2.4
-# 1.2.3
-# 1.2.2
-# 1.2.1
-# 1.1.2
-# 1.1.1
-# 1.0.1
-# 1.0.0
-# 0.7.1
-# 0.7.0
-# 0.6.6
-# 0.6.5
-# 0.6.5-2
-# 0.6.5-1
-# 0.6.4
-# 0.6
-# 0.6-3
-# 0.6-2
-# 0.6-1
-# 0.5.5
-# 0.5.5-4
-# 0.5.5-3
-# 0.5.5-2
-# 0.5.5-1
-# 0.5.4
-# 0.5.4-1
-# 0.5.3
-# 0.5.2
-# 0.5.2-2
-# 0.5.1
-# 0.5
-# 0.4.5
-# 0.4.4
-# 0.4.3
-# 0.4.2
-# 0.4.1
-# 0.4
-# 0.3.3
-# 0.3.2
-# 0.3.1
-# 0.2.1
-# 0.2
-# 0.1.2
-# 0.1.1
+version_tags <- dir("/rlib")
 
 create_ring <- function(tag) {
-  withr::local_temp_libpaths()
+  .libPaths(file.path("/rlib", tag))
 
   try({
-    pak::pak(paste0("igraph@", tag))
     library(igraph)
 
     # Old name for make_ring()
@@ -88,7 +24,7 @@ create_ring <- function(tag) {
   })
 }
 
-my_tags <- set_names(version_tags$name)
+my_tags <- set_names(version_tags)
 
 graphs <- map(map(my_tags, as.list), safely(callr::r, quiet = FALSE), func = create_ring, .progress = TRUE)
 
