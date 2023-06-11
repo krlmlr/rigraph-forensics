7f66ffc28c7c450dc88b3f74fcd157d15c3f1e11
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-11 08:32:19 +0200

    Create constructed files with script

---
+++ 04-construct.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+delta <- readRDS("igraph-versions.rds")
+
+fs::dir_create("old")
+
+# White lie
+delta$result[[1]][[10]]$myid <- delta$result[[2]][[10]]$myid
+
+code <-
+  delta |>
+  select(version, result) |>
+  deframe() |>
+  map(
+    constructive::construct,
+    constructive::opts_environment("new_environment", predefine = TRUE),
+    pipe = "magrittr"
+  ) |>
+  map(capture.output) |>
+  enframe() |>
+  mutate(value = map_chr(value, ~ paste0("  ", .x, collapse = "\n"))) |>
+  mutate(value = paste0(
+    "oldsample_", str_replace_all(name, fixed("."), "_"), " <- function() {\n",
+    value, "\n",
+    "}"
+  )) |>
+  mutate(expr = map(value, rlang::parse_expr)) |>
+  mutate(path = fs::path("old", paste0("old-", str_replace_all(name, fixed("."), "_"), ".R")))
+
+code
+
+code |>
+  select(text = value, path) |>
+  pmap(brio::write_lines)
